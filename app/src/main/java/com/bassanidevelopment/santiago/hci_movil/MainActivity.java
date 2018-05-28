fb1750217caba95976e7743ecc170404309636ec
BiancaR97 <britorto@itba.edu.ar>
2018-05-28 17:16:29 -0300

    Bottom Nav

--- app/src/main/java/com/bassanidevelopment/santiago/hci_movil/MainActivity.java
+++ app/src/main/java/com/bassanidevelopment/santiago/hci_movil/MainActivity.java
@@ -1,13 +1,53 @@
 package com.bassanidevelopment.santiago.hci_movil;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayBottomNav();
+    }
+
+    public void displayBottomNav(){
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Goes Home", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_rooms:
+                        startRooms();
+                        break;
+
+                    case R.id.action_routines:
+                        Toast.makeText(MainActivity.this, "Goes to Routines", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void startRooms(){
+        Intent intent = new Intent(this, RoomsView.class);
+        startActivity(intent);
+    }
+
+    public void startRotuines(){
+
     }
 }
