d791178d6b1113816a248f10d4359c9343434d99
BiancaR97 <britorto@itba.edu.ar>
2018-06-18 21:56:08 -0300

    Activities

---
+++ app/src/main/java/com/bassanidevelopment/santiago/hci_movil/HelpActivity.java
@@ -0,0 +1,12 @@
+package com.bassanidevelopment.santiago.hci_movil;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HelpActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
